03af7eb878829295794fc8b286ca804951527b7f
Thanos Psaroudas <thanos.psaroudas@gmail.com>
2025-02-11 19:05:21 +0100

    uploading files for non-dynamic test cases

--- src/tyndp_model_matching/data.jl
+++ src/tyndp_model_matching/data.jl
@@ -193,7 +193,7 @@ function construct_data_dictionary_2024(ntcs, arcs, capacity, nodes, demand, sce
     return data, nodal_data
 end
     
-function prepare_hourly_data!(data, nodal_data, hour, iteration, cable_data, cable_id, input_data_raw, i, number_of_iterations, reps, repetitions, j, starting_temperature) 
+function prepare_hourly_data!(data, nodal_data, hour) 
     for (l, load) in data["load"]
         node = load["node"]
         load["pd"] = nodal_data[node]["demand"][hour] / data["baseMVA"]
@@ -211,67 +211,6 @@ function prepare_hourly_data!(data, nodal_data, hour, iteration, cable_data, cab
         elseif gen["type"] == "ENS"
             gen["pmax"] = nodal_data[node]["demand"][hour] / data["baseMVA"]
         end
-    end
-
-    #########################MODIFICATIONS##########################
-
-    # Initialize admissible power and temperature of first hour of each time slice - cluster of simulation
-    if hour == repetitions[j][1]                       
-        for cable in cable_id
-            cable_data["$cable"]["$hour"]["P_adm"]["$iteration"] = 100
-            cable_data["$cable"]["$hour"]["temperature"]["$iteration"] = starting_temperature
-        end
-    end
-    
-    
-    if iteration > 1  # updating capacities starts after second iteration (based on the previous one)
-
-        for cable in cable_id
-
-            data["branch"]["$cable"]["rate_a"] = (1/100) * cable_data["$cable"]["$hour"]["P_adm"]["$(iteration-1)"] * input_data_raw["branch"]["$cable"]["rate_a"]
-            data["branch"]["$cable"]["rate_p"] = (1/100) * cable_data["$cable"]["$hour"]["P_adm"]["$(iteration-1)"] * input_data_raw["branch"]["$cable"]["rate_p"]
-            data["branch"]["$cable"]["rate_i"] = (1/100) * cable_data["$cable"]["$hour"]["P_adm"]["$(iteration-1)"] * input_data_raw["branch"]["$cable"]["rate_i"]
-            
-        end
-
-
-    elseif iteration == 1
-
-        if reps == 1        # indication that we are moving to a new timeslice, since reps counter initializes from 1 when a new timeslice starts
-            # Start from fixed capacities.
-            for cable in cable_id
-                data["branch"]["$cable"]["rate_a"] =  input_data_raw["branch"]["$cable"]["rate_a"]
-                data["branch"]["$cable"]["rate_p"] =  input_data_raw["branch"]["$cable"]["rate_p"]
-                data["branch"]["$cable"]["rate_i"] =  input_data_raw["branch"]["$cable"]["rate_i"] 
-            end
-
-        elseif reps != 1     # indication that we moved to a new prediction horizon but not a new timeslice
-            # update capacites based on converged results from previous prediction horizon.
-            for cable in cable_id
-                data["branch"]["$cable"]["rate_a"] = (1/100) * cable_data["$cable"]["$(repetitions[j][reps])"]["P_adm"]["$(number_of_iterations[j,reps-1])"] * input_data_raw["branch"]["$cable"]["rate_a"]
-                data["branch"]["$cable"]["rate_p"] = (1/100) * cable_data["$cable"]["$(repetitions[j][reps])"]["P_adm"]["$(number_of_iterations[j,reps-1])"] * input_data_raw["branch"]["$cable"]["rate_p"]
-                data["branch"]["$cable"]["rate_i"] = (1/100) * cable_data["$cable"]["$(repetitions[j][reps])"]["P_adm"]["$(number_of_iterations[j,reps-1])"] * input_data_raw["branch"]["$cable"]["rate_i"]
-            end
-
-        end
-
-    end
-
-
-    # Update all "first" hours of prediction horizon loops based on the result of final iteration of the previous hour (converged results)
-    # Also, do not update during the start of simulation or the start of a new time slice.        
-    
-    if hour == i && hour != repetitions[j][1]
-    
-        for cable in cable_id
-            cable_data["$cable"]["$hour"]["P_adm"]["$iteration"] = cable_data["$cable"]["$hour"]["P_adm"]["$(number_of_iterations[j,reps-1])"]
-            cable_data["$cable"]["$hour"]["temperature"]["$iteration"] = cable_data["$cable"]["$hour"]["temperature"]["$(number_of_iterations[j,reps-1])"]
-        end
-
-        # If we want to start each prediction loop "blind-folded" we could set T = 75 and P_adm = 100% at every "first" hour of the loop.
-        # This could combine with setting the capacity of first iteration equal to fixed capacity of cables.
-        # number_of_iterations: This variable refers to the final iteration of each hour (or of each prediction horizon loop)
-
     end
 
     return data
