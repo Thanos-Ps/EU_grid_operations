fde670b265528e38db9c468a65b643ca6329e65d
Thanos Psaroudas <thanos.psaroudas@gmail.com>
2025-03-18 18:25:14 +0100

    adapting post-processing file

--- scripts/post-processing.jl
+++ scripts/post-processing.jl
@@ -1,18 +1,20 @@
 using Plots
 
-cable_id = 16
+# Due to the temporal sampling, only 1 time slice will be plotted (at least for now), to ensure continuity of time.
+selected_cluster = 6
+selected_cable = cable_id[1]
 temperature_values = []
 power_values = []
 abs_power_values = []
 diff_values = []
 
-hours = 1:number_of_hours-1
+hours = 1:prediction_horizon -1 # by doing that we are not plotting the last hour because the difference between pabs and p_to is large! (actual issue is not resloved)
 
-for hour in hours
+for hour in 1:prediction_horizon-1
           
-    T = result["solution"]["nw"]["$hour"]["branch"]["$cable_id"]["Temperature"]
-    p_to = 100*result["solution"]["nw"]["$hour"]["branch"]["$cable_id"]["pt"]/input_data["branch"]["$cable_id"]["rate_a"]
-    p_abs = 100*result["solution"]["nw"]["$hour"]["branch"]["$cable_id"]["p_abs"]/input_data["branch"]["$cable_id"]["rate_a"]
+    T = result["$selected_cluster"]["solution"]["nw"]["$hour"]["branch"]["$selected_cable"]["Temperature"]
+    p_to = 100*result["$selected_cluster"]["solution"]["nw"]["$hour"]["branch"]["$selected_cable"]["pt"]/input_data["branch"]["$selected_cable"]["rate_a"]
+    p_abs = 100*result["$selected_cluster"]["solution"]["nw"]["$hour"]["branch"]["$selected_cable"]["p_abs"]/input_data["branch"]["$selected_cable"]["rate_a"]
     difference = p_abs - abs(p_to)
     push!(temperature_values, T)
     push!(power_values, p_to)
@@ -21,10 +23,10 @@ for hour in hours
 
 end
 
-p1 = plot(hours, power_values, xlabel="Hour", ylabel="Rating [%]", title="Loading of Cable $cable_id", legend = false)
-#p1 = plot!(hours, abs_power_values, xlabel="Hour", ylabel="Rating [%]", title="Loading of Cable $cable_id", legend = false)
-p2 = plot(hours, abs_power_values, xlabel="Hour", ylabel="Rating [%]", title="Abs Power of Cable $cable_id", legend = false)
-p3 = plot(hours, temperature_values, xlabel="Hour", ylabel="[degC]", title="Temperature of Cable $cable_id", legend = false)
+p1 = plot(hours, power_values, xlabel="Hour", ylabel="Rating [%]", title="Loading of Cable $selected_cable", legend = false)
+#p1 = plot!(hours, abs_power_values, xlabel="Hour", ylabel="Rating [%]", title="Loading of Cable $selected_cable", legend = false)
+p2 = plot(hours, abs_power_values, xlabel="Hour", ylabel="Rating [%]", title="Abs Power of Cable $selected_cable", legend = false)
+p3 = plot(hours, temperature_values, xlabel="Hour", ylabel="[degC]", title="Temperature of Cable $selected_cable", legend = false)
 p4 = plot(hours, diff_values, xlabel="Hour", title="P_abs - |P_to|", legend = false)
 plot(p1,p4, p3, layout =(3,1))
 
